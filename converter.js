a643920c7c111684551c0c9f6fa5c51444f8b74b
Meg Scholl <meghan.e.scholl@gmail.com>
2018-02-15 15:58:24 -0600

    working on js

--- converter.js
+++ converter.js
@@ -1,10 +1,26 @@
-function toCelsius () {
+console.log("I'm working");
 
+let celcius = document.getElementById("celcius");
+let fahrenheit = document.getElementById("fahrenheight");
+
+function toCelsius(event){ 
+  if(event.keycode === 13 && celcius.checked === true) {
+    toCelcius();
+    console.log("toCelcius is working");
+  }  else{
+    alert("please enter temperature value");
+}};
+
+function toFahrenheit(event) { // if user chooses C to convert to F, then this function will run
+  if(event.keycode === 13 && fahrenheit.checked === true) {
+    toFahrenheit();
+    console.log("toCelcius is working");
+  }  else{
+    alert("please enter temperature value");
+  console.log("converting to C");
 }
 
-function toFahrenheit () {
-
-}
+//display what the user inputted to the div
 
 // Get a reference to the button element in the DOM
 var button = document.getElementById("converter");
